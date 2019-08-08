823d617cff689c16eb81f2d08a6aec2346ecfe87
Takahiro Utagawa <ta.uta07@gmail.com>
2019-08-08 01:08:18 +0000

    image ubuntu

--- hello-world/background.sh
+++ hello-world/background.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #rootディレクトへ移動
-cd /root/
+cd $HOME
 
 #AWX公式githubから最新版を取得
 git clone https://github.com/ansible/awx.git
 
 #インストーラディレクトリへ移動
-cd /root/awx/installer
+cd $HOME/awx/installer
 
 #AWXをインストール
-ansible-playbook -i inventory install.yml
+sudo ansible-playbook -i inventory install.yml
 
 #　インストール完了を表示
 echo "AWX Install Complete"
