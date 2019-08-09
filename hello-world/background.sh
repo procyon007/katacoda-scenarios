daf37fb8fdc8f876d6f59ef1b26223ab5b831cb3
Takahiro Utagawa <ta.uta07@gmail.com>
2019-08-09 02:51:12 +0000

    background.sh change

--- hello-world/background.sh
+++ hello-world/background.sh
@@ -9,9 +9,21 @@ git clone https://github.com/ansible/awx.git
 #インストーラディレクトリへ移動
 cd $HOME/awx/installer
 
-#ansible インストール
+#Docker インストール
+sudo apt-get remove docker.io 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+cd /usr/lib/python3/dist-packages
+sudo ln -s apt_pkg.cpython-{35m,37m}-x86_64-linux-gnu.so
+sudo add-apt-repository \
+	   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	      $(lsb_release -cs) \
+	         stable"
 sudo apt-get update
-sudo pip install ansible
+sudo apt-get install -y docker-ce
+
+#ansible インストール
+sudo pip3 install setuptools
+sudo pip3 install ansible
 
 #AWXをインストール
 sudo ansible-playbook -i inventory install.yml
