030f6019ef442f0c901fd93ea9ababa063afda9d
Takahiro Utagawa <ta.uta07@gmail.com>
2019-08-09 04:09:21 +0000

    test4

---
+++ hello-world/foreground.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#rootディレクトへ移動
+cd $HOME
+
+#AWX公式githubから最新版を取得
+git clone https://github.com/ansible/awx.git
+
+#インストーラディレクトリへ移動
+cd $HOME/awx/installer
+
+#Docker インストール
+sudo apt-get remove docker.io 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+cd /usr/lib/python3/dist-packages
+sudo ln -s apt_pkg.cpython-{35m,37m}-x86_64-linux-gnu.so
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+#ansible インストール
+sudo pip3 install setuptools
+sudo pip3 install ansible
+
+#AWXをインストール
+sudo ansible-playbook -i inventory install.yml
+
+#　インストール完了を表示
+echo "AWX Install Complete"
