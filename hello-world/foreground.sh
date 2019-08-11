3b395280a3cd2395504ea244788968e0b4cfa68e
Takahiro Utagawa <ta.uta07@gmail.com>
2019-08-11 12:03:49 +0900

    test

--- hello-world/foreground.sh
+++ hello-world/foreground.sh
@@ -11,15 +11,15 @@ cd $HOME/awx/installer
 
 #Docker インストール
 #sudo apt-get -y remove docker.io 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-cd /usr/lib/python3/dist-packages
-sudo ln -s apt_pkg.cpython-{35m,37m}-x86_64-linux-gnu.so
-sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#cd /usr/lib/python3/dist-packages
+#sudo ln -s apt_pkg.cpython-{35m,37m}-x86_64-linux-gnu.so
+#sudo add-apt-repository \
+#  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#  $(lsb_release -cs) \
+#  stable"
+#sudo apt-get update
+#sudo apt-get install -y docker-ce
 
 #ansible インストール
 sudo pip3 install setuptools
