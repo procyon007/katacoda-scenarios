2ddf1814b3a1a611560b5f535f0b8122ca993a80
Takahiro Utagawa <ta.uta07@gmail.com>
2019-08-30 15:09:48 +0900

    AWX Playground..etc

---
+++ AWX_Playground/foreground.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DOCKER_IMAGE="procyon07/kata_image"
+
+#rootディレクトへ移動
+cd $HOME
+
+#AWX公式githubから最新版を取得
+git clone https://github.com/ansible/awx.git
+
+#インストーラのディレクトリへ移動
+cd $HOME/awx/installer
+
+#必要モジュールを インストール
+sudo pip3 install setuptools
+sudo pip3 install ansible
+sudo pip2.7 install docker
+sudo pip2.7 install docker-compose
+sudo pip2.7 install requests
+
+#AWXをインストール
+sed -i s/"#project_data_dir"/project_data_dir/g inventory
+sudo ansible-playbook -i inventory install.yml
+
+#　インストール完了を表示
+echo "AWX Install Complete"
+
+
+
+for i in {1..2}
+do
+  docker run --net=awxcompose_default --rm=true --name=host$i $DOCKER_IMAGE /sbin/init &
+done
---
+++ AWX_Playground/step1.md
@@ -0,0 +1,3 @@
+本コースでは、以下図のような環境を提供しております。  
+  
+![Network](https://raw.githubusercontent.com/procyon007/katacoda-scenarios/master/Network%20Diagram.png)
