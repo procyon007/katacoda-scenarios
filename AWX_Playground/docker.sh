b3dac8c131c354b91ceff374f20b4da33eb1bd0a
Takahiro Utagawa <ta.uta07@gmail.com>
2019-09-15 21:55:14 +0900

    fg shell delete

--- AWX_Playground/docker.sh
+++ AWX_Playground/docker.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 echo "AWX setup conplete"
 echo "host1/2 setup start"
-DOCKER_IMAGE="procyon07/kata_image"
-
-for i in {1..2}
-do
-  sudo docker run --net=awxcompose_default --rm=true --name=host$i $DOCKER_IMAGE
-done
