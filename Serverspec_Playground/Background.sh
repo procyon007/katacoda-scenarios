fe014a394dace5c9f433c30b50a15dce2d91b16f
Takahiro Utagawa <ta.uta07@gmail.com>
2019-11-20 15:03:18 +0900

    test

--- Serverspec_Playground/Background.sh
+++ Serverspec_Playground/Background.sh
@@ -36,6 +36,6 @@ for i in 1 2
 do
     docker run -d --security-opt label:disable --net=sp_play --rm=true --name=host$i $DOCKER_IMAGE /sbin/init
     sleep 10
-    IPADDR=`docker inspect host${i}  | jq -r ".[0].NetworkSettings.IPAddress"`
+    IPADDR=`docker inspect host$i  | jq -r ".[0].NetworkSettings.IPAddress"`
     echo node-${i} ansible_ssh_host=${IPADDR:?} ansible_ssh_user=ansible ansible_ssh_pass=password123 >> inventory
 done
