9c5d58f462b09ecfd679cb0995b72353bef9e712
Takahiro Utagawa <ta.uta07@gmail.com>
2019-11-24 04:01:43 +0900

    ansible check tst

---
+++ Serverspec_Playground/foreground.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#変数設定
+ansible="/etc/ansible/hosts"
+
+#Setup開始を通知
+echo "環境のセットアップを開始、完了までお待ち下さい"
+
+#ansible setup完了確認
+while :
+rpm -qa | grep ansible 
+do
+  if [ -a $ansible ]; then
+    echo "ansibleのインストール完了"
+    break
+  fi
+done
