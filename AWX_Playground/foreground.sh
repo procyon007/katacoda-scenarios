fca2d719795f3332e76eee024eee8e4b52ea8e58
procyon007 <ta.uta07@gmail.com>
2021-04-12 04:53:57 -0400

    test

--- AWX_Playground/foreground.sh
+++ AWX_Playground/foreground.sh
@@ -6,7 +6,7 @@ DOCKER_IMAGE="procyon07/kata_image"
 cd $HOME
 
 #AWX公式githubから最新版を取得
-git clone https://github.com/ansible/awx.git -b 17.0.1 --depth 1
+git clone https://github.com/ansible/awx.git -b 17.0.1
 
 #インストーラのディレクトリへ移動
 cd $HOME/awx/installer
